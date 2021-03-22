e0ffac9f7ee923175a7e2508f440a473246b25e9
0x112a <1436063244@qq.com>
2021-03-22 21:36:28 +0800

    add 0x016

--- 0x016/main.go
+++ 0x016/main.go
@@ -22,9 +22,61 @@ func main() {
 		fmt.Println(threeSumClosest(n, t))
 	}
 }
-func threeSumClosest(nums []int, target int) int {
-	fmt.Println(bubsort(nums))
-	return 0
+func threeSumClosest(nums []int, target int) (ans int) {
+	nums = bubsort(nums)
+	l := len(nums)
+	local := -1
+	left, right := 0, l-1
+	for i := l / 2; left != right-1; {
+		fmt.Println("i:", i)
+		if nums[i] == target {
+			local = i
+			left = i - 1
+			right = i + 1
+			break
+		} else if nums[i] < target {
+			left = i
+			i = (right + left) / 2
+		} else {
+			right = i
+			i = (right + left) / 2
+		}
+	}
+	n := 0
+	if local != -1 {
+		ans += nums[local]
+		n++
+	}
+	ads := func(i int) int {
+		if i < 0 {
+			return -1 * i
+		}
+		return i
+	}
+	for n < 3 {
+		fmt.Println("left", left, "right", right)
+		if left >= 0 && right <= l-1 {
+			if ads(nums[left]-target) < ads(nums[right]-target) {
+				ans += nums[left]
+				n++
+				left--
+			} else {
+				ans += nums[right]
+				n++
+				right++
+			}
+		} else if left < 0 {
+			ans += nums[right]
+			n++
+			right++
+		} else {
+			ans += nums[left]
+			n++
+			left--
+		}
+	}
+
+	return ans
 }
 
 func bubsort(nums []int) (ans []int) {
