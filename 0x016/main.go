704807a68dd479158cd2c0000797418fa8612abb
0x112a <1436063244@qq.com>
2021-03-24 16:26:41 +0800

    add code

--- 0x016/main.go
+++ 0x016/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -25,57 +26,47 @@ func main() {
 func threeSumClosest(nums []int, target int) (ans int) {
 	nums = bubsort(nums)
 	l := len(nums)
-	local := -1
-	left, right := 0, l-1
-	for i := l / 2; left != right-1; {
-		fmt.Println("i:", i)
-		if nums[i] == target {
-			local = i
-			left = i - 1
-			right = i + 1
-			break
-		} else if nums[i] < target {
-			left = i
-			i = (right + left) / 2
-		} else {
-			right = i
-			i = (right + left) / 2
-		}
-	}
-	n := 0
-	if local != -1 {
-		ans += nums[local]
-		n++
-	}
-	ads := func(i int) int {
+	ans = math.MaxInt32
+	abs := func(i int) int {
 		if i < 0 {
 			return -1 * i
 		}
 		return i
 	}
-	for n < 3 {
-		fmt.Println("left", left, "right", right)
-		if left >= 0 && right <= l-1 {
-			if ads(nums[left]-target) < ads(nums[right]-target) {
-				ans += nums[left]
-				n++
-				left--
-			} else {
-				ans += nums[right]
-				n++
-				right++
+	direction := func(current int) bool {
+		if abs(current-target) < abs(ans-target) {
+			ans = current
+		}
+		if current > target {
+			return true
+		}
+		return false
+	}
+	for i := 0; i < l; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left, right := i+1, l-1
+		for left < right {
+			current := nums[i] + nums[left] + nums[right]
+			if current == target {
+				return current
 			}
-		} else if left < 0 {
-			ans += nums[right]
-			n++
-			right++
-		} else {
-			ans += nums[left]
-			n++
-			left--
+			if direction(current) {
+				temp := right - 1
+				if left < temp && nums[temp] == nums[right] {
+					temp--
+				}
+				right = temp
+			} else {
+				temp := left + 1
+				if right > temp && nums[temp] == nums[left] {
+					temp++
+				}
+				left = temp
+			}
 		}
 	}
-
 	return ans
 }
 
