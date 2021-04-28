dfd042009287d795115236a53d3d0c7d66c8260f
0x112a <1436063244@qq.com>
2021-04-28 22:49:15 +0800

    add 0x633

---
+++ 0x633/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+func judgeSquareSum(c int) bool {
+	left, right := 0, int(math.Sqrt(float64(c)))
+
+	for left <= right {
+		sum := left*left + right*right
+		if sum == c {
+			return true
+		} else if sum > c {
+			right--
+		} else {
+			left++
+		}
+	}
+	return false
+}
