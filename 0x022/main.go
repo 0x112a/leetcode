902956fd55116e8207ff79c5429217ecc1877614
0x112a <1436063244@qq.com>
2021-06-25 21:14:29 +0800

    updata some file

--- 0x022/main.go
+++ 0x022/main.go
@@ -3,5 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+
+	var n int
+	fmt.Scanf("%d", &n)
+	r := generateParenthesis(n)
+	fmt.Println(r)
+}
+
+var result []string
+
+func generateParenthesis(n int) []string {
+	if n == 0 {
+		return []string{}
+	} else if n == 1 {
+		return []string{"()"}
+	}
+
+	getParenthesis("", n, n)
+
+	return result
+}
+
+func getParenthesis(str string, left int, right int) {
+	if left == 0 && right == 0 {
+		result = append(result, str)
+		return
+	}
+
+	if left == right {
+		getParenthesis(str+"(", left-1, right)
+	} else if left < right {
+		if left > 0 {
+			getParenthesis(str+"(", left-1, right)
+		}
+		getParenthesis(str+")", left, right-1)
+	}
 }
