1fbe64721abeb6c3869c4183d9d57ec12f4cb557
0x112a <1436063244@qq.com>
2021-07-04 15:25:31 +0800

    updata 0x088

--- 0x088/main.go
+++ 0x088/main.go
@@ -17,3 +17,24 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	}
 	sort.Ints(nums1)
 }
+// 逆双指针
+func Merge(nums1 []int,m int,nums2 []int, n int)  {
+	p1,p2 := m-1,n-1
+	cur := m+n-1
+	for p1 >= 0 || p2 >=0 {
+		if p1 < 0 {
+			nums1[cur] = nums2[p2]
+			p2--
+		}else if p2 < 0{
+			nums1[cur] = nums1[p1]
+			p1--
+		}else if nums1[p1] > nums2[p2] {
+			nums1[cur] = nums1[p1]
+			p1--
+		}else if nums1[p1] <= nums2[p2]{
+			nums1[cur] = nums2[p2]
+			p2--
+		}
+		cur--
+	}
+}
