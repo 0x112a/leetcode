08bf02c5f8d36958b7faaf22626732e9a6571187
0x112a <1436063244@qq.com>
2021-07-04 15:25:12 +0800

    add 0x001

---
+++ 0x001/towSum.go
@@ -0,0 +1,30 @@
+package _x001
+
+import "sort"
+
+func twoSum(nums []int, target int) []int{
+	sort.Ints(nums)
+	n := len(nums)
+	small,big := 0,n-1
+
+	for nums[small]+nums[big] != target{
+	//for small < big{
+		if nums[small]+nums[big] < target {
+			small++
+		}else {
+			big--
+		}
+	}
+	return []int{small,big}
+}
+
+func TowSum(nums []int,target int) []int {
+	hashTable := map[int]int{}
+	for i,v := range nums{
+		if p,ok := hashTable[target-v]; ok{
+			return []int{i,p}
+		}
+		hashTable[v]=i
+	}
+	return nil
+}
